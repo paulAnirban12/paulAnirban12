2f3f8ffcc5a5c5ef7bdc2500aa8c8f69eafe79f5
paulAnirban12 <142673746+paulAnirban12@users.noreply.github.com>
2025-04-07 20:09:03 +0600

    Delete Data Structure directory

--- Data Structure/Searching/find_number_based_on_location.py
+++
@@ -1,8 +0,0 @@
-arr = list(map(int, input("Numbers: ").split(',')))
-size = len(arr)
-index = int(input("Index_Number:"))
-
-for i in range(size):
-    if i == index:
-        print(f"{arr[i]} is the {index}th number of the list.")
-        break
